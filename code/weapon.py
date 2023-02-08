44e95289b8f575511ebfda3c8605c85bb9f51f86
Razkin1234 <120375659+Razkin1234@users.noreply.github.com>
2023-02-08 21:50:55 +0200

    Update weapon.py

--- code/weapon.py
+++ code/weapon.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 
 class Weapon(pygame.sprite.Sprite):
@@ -19,25 +18,3 @@ class Weapon(pygame.sprite.Sprite):
             self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10,0))
         else: #for up status
             self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(-18,0))
-=======
-import pygame
-
-class Weapon(pygame.sprite.Sprite):
-    def __init__(self,player,groups):
-        super().__init__(groups)
-        direction = player.status.split('_')[0]
-        self.sprite_type ='weapon'
-        #graphics
-        full_path = f'../graphics/weapons/{player.weapon}/{direction}.png'
-        self.image = pygame.image.load(full_path).convert_alpha()
-
-        #placement
-        if direction == 'right':
-            self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0,5))#where the weapon sprite will be
-        elif direction == 'left':
-            self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(0,5))
-        elif direction =='down':
-            self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10,0))
-        else: #for up status
-            self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(-18,0))
->>>>>>> 48c8e0dd190a305b0ccf34767036f7e6d7254801
